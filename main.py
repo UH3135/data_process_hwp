bef2c26b0e9d72f7d07cc8fb33d3954e812ded1d
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-02-14 21:23:38 +0900

    STYLE: 여러 코드 테스트 및 스타일 변경

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from utils.converter import convert_all_hwp2md
-from utils.ocr_models import get_all_easy_ocr
-from utils.finder import get_hwp_filenames, get_jpg_filenames
+from utils.converter import convert_all_hwp2md, convert_all_hwp2pdf
+from utils.ocr_models import extract_text_with_ocr
 from etc.logger import init_logger
 import os
 
 logger = init_logger(__file__, 'DEBUG')
 
-data_path = os.path.abspath("assets/output/acadj_10/bindata/BIN0001.jpg")
+hwp_path = os.path.abspath("assets/OneDrive_2025-02-14/acadj_10.hwp")
 data_dir = os.path.abspath('assets')
+md_path = os.path.abspath('assets/output/acadj_1/acadj_1.md')
+jpg_path = os.path.abspath('assets/output/acadj_10/bindata/BIN0001.jpg')
 
-# convert_all_hwp2md()
+
+# hwp_to_pdf(hwp_path)
+convert_all_hwp2md(data_dir)
+# convert_all_hwp2pdf(data_dir)
 # print(get_jpg_filenames(data_dir))
-get_all_easy_ocr(data_dir)
+# get_all_easy_ocr(data_dir)
--- utils/converter/__init__.py
+++ utils/converter/__init__.py
@@ -1,2 +1,1 @@
 from utils.converter.convert_all import convert_all_hwp2md, convert_all_hwp2pdf
-from utils.converter.converter import hwp_to_pdf
