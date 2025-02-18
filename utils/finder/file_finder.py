45db4b54f3fbfd9ffe8cd4e0bc62313b9f472cf4
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-02-18 22:45:43 +0900

    FIX: 파일을 제대로 못찾던 문제 해결

--- utils/finder/file_finder.py
+++ utils/finder/file_finder.py
@@ -11,15 +11,15 @@ def get_filenames_with_type(directory: str, type: str) -> List[str]:
         logger.error(f"Error:'{directory}'가 존재하지 않습니다.")
         return []
 
-    files = []
+    found_files = []
     for root, _, files in os.walk(directory):
-        files.extend([os.path.join(root, f) for f in files if f.lower().endswith(f".{type}")])
+        logger.info(f"{root}를 확인하고 있습니다")    
+        found_files.extend([os.path.join(root, f) for f in files if f.lower().endswith(f".{type}")])
 
-    if not files:
+    if not found_files:
         logger.error(f"해당 {directory}에 {type} 파일이 없습니다.")
         return []
 
-    logger.info(f"{len(files)}개의 {type} 파일을 찾았습니다.")
+    logger.info(f"{len(found_files)}개의 {type} 파일을 찾았습니다.")
 
-    return files
-
+    return found_files
